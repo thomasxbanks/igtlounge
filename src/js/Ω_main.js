2eb65629d799a6c790162a0f2677424690c3798e
thomasxbanks <thomas.banks@wearecube3.com>
2018-04-04 14:42:30 +0100

    WIP - Components Library

--- src/js/Ω_main.js
+++ src/js/Ω_main.js
@@ -2,7 +2,8 @@
 
 window.onload = () => {
   document.documentElement.classList.remove('no-js'); // Tells us that JS isn't disabed in the browser.
-  if (!window.location.href.indexOf('local') > -1) {
+
+  if (window.location.href.indexOf('local') <= 0) {
     // In case we forget to remove the .redline class prior to production,
     // this will remove it for us.
     // Presumes we're using a xxx.local or localhost:PORT syntax development URI
