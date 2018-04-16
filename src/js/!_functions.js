5b08e2a461778422da5bd1007a4101fb7719da10
thombanks <thomas.banks@wearecube3.com>
2018-04-16 16:48:49 +0100

    IGTD-50 - WIP - game thumbnails

--- src/js/!_functions.js
+++ src/js/!_functions.js
@@ -76,3 +76,11 @@ const closeModal = () => {
   document.querySelector('.modal__container').setAttribute('data-state', 'not-active');
   document.documentElement.style.overflow = ``;
 };
+
+const viewGameDetails = (target) => {
+  const thumbnails = document.querySelectorAll('.thumbnail');
+  thumbnails.forEach((thumbnail) => {
+    thumbnail.setAttribute('data-state', 'not-active');
+  });
+  document.querySelector(`[data-target="${target}"]`).setAttribute('data-state', 'is-active');
+};
