bc14755dc3511e9898b2c8937394b0f323ad5a0c
thombanks <thomas.banks@wearecube3.com>
2018-04-17 12:36:19 +0100

    IGTD-50 - WIP - mobile sidebar nav

--- src/js/!_functions.js
+++ src/js/!_functions.js
@@ -84,3 +84,13 @@ const viewGameDetails = (target) => {
   });
   document.querySelector(`[data-target="${target}"]`).setAttribute('data-state', 'is-active');
 };
+
+const openSidebar = () => {
+  document.querySelector('.sidebar').setAttribute('data-state', 'is-active');
+  document.documentElement.style.overflow = `hidden`;
+};
+
+const closeSidebar = () => {
+  document.querySelector('.sidebar').setAttribute('data-state', 'not-active');
+  document.documentElement.style.overflow = ``;
+};
