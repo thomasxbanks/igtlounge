567bf5027f0e22222487a5b86626a3e9b501c84e
thombanks <thomas.banks@wearecube3.com>
2018-04-25 14:48:21 +0100

    IGTD-50 - WIP - validation & homepage

--- src/js/!_functions.js
+++ src/js/!_functions.js
@@ -41,8 +41,11 @@ const validate = (event) => {
   var valid = thisInput.checkValidity();
   if (valid) {
     thisInputContainer.querySelector('.input-validation-message').style.display = `none`;
+    thisInputContainer.querySelector('.input-validation-message').innerHTML = ``;
   } else {
     thisInputContainer.querySelector('.input-validation-message').style.display = `block`;
+    thisInputContainer.querySelector('.input-validation-message').innerHTML =
+      thisInput.validationMessage;
   }
 };
 
